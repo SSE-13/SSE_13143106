3fe860204d04e5315e968a05c8419292ade691a6
13143106 <1561372170@qq.com>
2016-03-12 23:09:06 +0800

    hw2

---
+++ canvas_oop/canvas_oop.js
@@ -0,0 +1,121 @@
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+/**
+ * 基类，负责处理x,y,rotation 等属性
+ */
+var DisplayObject = (function () {
+    function DisplayObject() {
+        this.x = 0;
+        this.y = 0;
+        this.rotation = 0;
+    }
+    DisplayObject.prototype.draw = function (context) {
+        context.save();
+        context.rotate(this.rotation);
+        context.translate(this.x, this.y);
+        this.render(context);
+        context.restore();
+    };
+    DisplayObject.prototype.render = function (context) {
+    };
+    return DisplayObject;
+}());
+var Bitmap = (function (_super) {
+    __extends(Bitmap, _super);
+    function Bitmap() {
+        _super.apply(this, arguments);
+    }
+    Bitmap.prototype.render = function (context) {
+        var image = imagePool[this.source];
+        if (image) {
+            context.drawImage(image, 0, 0);
+        }
+        else {
+            context.font = "20px Arial";
+            context.fillStyle = '#000000';
+            context.fillText('错误的URL', 0, 20);
+        }
+    };
+    return Bitmap;
+}(DisplayObject));
+var Rect = (function (_super) {
+    __extends(Rect, _super);
+    function Rect() {
+        _super.apply(this, arguments);
+        this.width = 100;
+        this.height = 100;
+        this.color = '#905546';
+    }
+    Rect.prototype.render = function (context) {
+        context.fillStyle = this.color;
+        context.fillRect(0, 0, this.width, this.height);
+    };
+    return Rect;
+}(DisplayObject));
+var TextField = (function (_super) {
+    __extends(TextField, _super);
+    function TextField() {
+        _super.apply(this, arguments);
+    }
+    TextField.prototype.render = function (context) {
+        context.font = "20px Arial";
+        context.fillStyle = '#ffffff';
+        context.fillText('PLAY', 544, 320);
+    };
+    return TextField;
+}(DisplayObject));
+function drawQueue(queue) {
+    for (var i = 0; i < renderQueue.length; i++) {
+        var displayObject = renderQueue[i];
+        displayObject.draw(context);
+    }
+}
+var imagePool = {};
+function loadResource(imageList, callback) {
+    var count = 0;
+    imageList.forEach(function (imageUrl) {
+        var image = new Image();
+        image.src = imageUrl;
+        image.onload = onLoadComplete;
+        image.onerror = onLoadError;
+        function onLoadComplete() {
+            imagePool[imageUrl] = image;
+            count++;
+            if (count == imageList.length) {
+                callback();
+            }
+        }
+        function onLoadError() {
+            alert('资源加载失败:' + imageUrl);
+        }
+    });
+}
+var canvas = document.getElementById("game");
+var context = canvas.getContext("2d");
+var rect = new Rect();
+rect.width = 1080;
+rect.height = 720;
+rect.color = '#905546';
+var rect2 = new Rect();
+rect2.width = 140;
+rect2.height = 55;
+rect2.x = 508;
+rect2.y = 284;
+rect2.color = '#44d723';
+var text = new TextField();
+text.x = 10;
+var bitmap = new Bitmap();
+bitmap.source = 'bitmap.png';
+bitmap.x = 300;
+bitmap.y = 42;
+//渲染队列
+var renderQueue = [rect, rect2, text, bitmap];
+//资源加载列表
+var imageList = ['bitmap.png'];
+//先加载资源，加载成功之后执行渲染队列
+loadResource(imageList, function () {
+    drawQueue(renderQueue);
+});
